090812ebbe360312fa345d89363c3efd5a524716
Gerben Venekamp <venekamp@gmail.com>
2019-01-02 22:39:08 +0100

    Improve readability of CO cards

--- frontend/src/components/CORenderAsCards.vue
+++ frontend/src/components/CORenderAsCards.vue
@@ -2,28 +2,38 @@
   <VContainer grid-list-md text-cs-center fluid>
     <VLayout row>
       <VFlex xs6 v-for="(CO, index) in COs" :key="index">
-        <VCard>
-          <VResponsive :aspect-ratio="5/3">
-            <VImg :src="CO.logo"></VImg>
+        <VCard class="card">
+          <VCardMedia class="card-media">
+            <VImg :src="CO.logo" contain height="100px"></VImg>
+          </VCardMedia>
 
-            <VCardTitle primary-title class="subheading font-weight-medium">
-              {{ CO.name }}
-            </VCardTitle>
+          <VCardTitle primary-title class="subheading font-weight-medium">
+            {{ CO.name }}
+        </VCardTitle>
 
-            <VCardText>
-              <div class="description">
-                {{ CO.description }}
-              </div>
-              <div>
-                <span class="font-weight-medium datelabel">start date:</span>
-                <span class="font-italic">{{ CO.start }}</span>
-              </div>
-              <div>
-                <span class="font-weight-medium datelabel">end date:</span>
-                <span class="font-italic">{{ CO.end }}</span>
-              </div>
-            </VCardText>
-          </VResponsive>
+          <hr class="card-element-separator" />
+
+          <VCardText>
+            <VContainer fluid pa-0>
+              <VLayout row>
+                <VFlex xs12 class="card-description">
+                  {{ CO.description }}
+                </VFlex>
+              </VLayout>
+
+              <VLayout row>
+                <VFlex xs6 class="font-weight-medium card-date-label">
+                  <div>start date:</div>
+                  <div>end date:</div>
+                </VFlex>
+                <VFlex xs6 class="font-italic">
+                  <div>{{ CO.start }}</div>
+                  <div>{{ CO.end }}</div>
+                </VFlex>
+              </VLayout>
+
+            </VContainer>
+          </VCardText>
         </VCard>
       </VFlex>
     </VLayout>
@@ -37,9 +47,23 @@ export default {
 </script>
 
 <style scope lang="stylus">
-.description
-  color: #484850
+.card
+  width: 24ex
 
-.datelabel
+.card-media
+  padding: 0 1em
+
+.card-description
+  color: #484850
+  font-size: 13px
+  height: 7.5em
+  overflow: auto
+  margin-bottom: 1ex
+
+.card-date-label
   color: #444
+
+.card-element-separator
+  margin 0 1em
+  border: 1px solid #bbb
 </style>
